8848429e0a2c82751893470b5e5ecc09247279b2
neyano <brn80jp@yahoo.co.jp>
2016-01-30 02:50:40 +0900

    キャラクタを追加しました。

---
+++ Assets/Scripts/AttackControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //ブロックに当たったらブロックを削除
+        if (coll.gameObject.tag == "Block")
+        {
+            Destroy(coll.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CharaController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharaController : MonoBehaviour
+{
+
+    // 変数の定義と初期化
+	public float flap;
+	public float scroll;
+
+    //ショット
+    public GameObject prefabShot;
+
+    private bool flagJamp = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * scroll);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * scroll);
+        }
+
+        // スペースキーが押されたら
+        if (Input.GetKeyDown("space") && flagJamp == false)
+        {
+            // 落下速度をリセット
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            // (0,1)方向に瞬間的に力を加えて跳ねさせる
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * flap, ForceMode2D.Impulse);
+
+            flagJamp = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Vector3 pos = transform.position;
+            pos.x += 1f;
+            pos.y += 2f;
+            GameObject shot = Instantiate(prefabShot, pos, transform.rotation) as GameObject;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        flagJamp = false;
+    }
+}
